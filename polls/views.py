61d0c7b52f876f6e1c67f64b43169ad1cadaec0b
Srajan Gupta <srajan.oel@gmail.com>
2020-12-25 00:10:27 +0530

    Corrected views

--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,7 @@ class IndexView(generic.ListView):
     template_name='polls/index.html'
     context_object_name='latest_question_list'
 
-    def def get_queryset(self):
+    def get_queryset(self):
         return Question.objects.order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
